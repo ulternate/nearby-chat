47902963a8c87b40ea83f6c06c9ab59617447941
ulternate <contact@danielcswain.com>
2016-08-03 17:30:47 +0800

    Removed Chat actions from MainActivity and reverted to a simple layout with FAB and Snackbar

--- app/src/main/java/com/danielcswain/nearbychat/MainActivity.java
+++ app/src/main/java/com/danielcswain/nearbychat/MainActivity.java
@@ -1,29 +1,15 @@
 package com.danielcswain.nearbychat;
 
-import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.EditText;
-import android.widget.ImageButton;
-
-import java.util.ArrayList;
-import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-
-    ArrayList<Message> mMessages;
-    EditText mTextField;
-    RecyclerView.Adapter mMessageRecyclerAdapter;
-    RecyclerView mMessagesRecyclerView;
-    RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,52 +18,14 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        // Get the messages
-        mMessages = new ArrayList<>();
-        mMessages.add(new Message("ulternate", "This is a message from me", true));
-        mMessages.add(new Message("kenneth", "This is a message from someone else", false));
-        mMessages.add(new Message("ulternate", "This is another message from me", true));
-        mMessages.add(new Message("ulternate", "This is a really, really, really, really, really, really, really, really, really, really, really, really, really, really, really, really, really, really, really, really, really, really, really, really, really, really, really long message", true));
-
-        // Get a reference to the RecyclerView and set the recycler view to have fixed layout size
-        // (as the layout is already full screen)
-        mMessagesRecyclerView = (RecyclerView) findViewById(R.id.messages_list);
-        mMessagesRecyclerView.setHasFixedSize(true);
-
-        // Using a stock linear layout manager for the RecyclerView
-        mLayoutManager = new LinearLayoutManager(this);
-        mMessagesRecyclerView.setLayoutManager(mLayoutManager);
-
-        // Set up the RecyclerView Adapter with the temporary data set and assign it to the RecyclerView
-        mMessageRecyclerAdapter = new MessageAdapter(mMessages);
-        mMessagesRecyclerView.setAdapter(mMessageRecyclerAdapter);
-
-        // Get the message send views
-        mTextField = (EditText) findViewById(R.id.text_entry_field);
-        ImageButton submitButton = (ImageButton) findViewById(R.id.message_send_button);
-
-        submitButton.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Get the edit text field's text
-                String message = mTextField.getText().toString();
-                if (!message.isEmpty()){
-                    // Randomly choose if the message is from the user or mimic being received
-                    Random rand = new Random();
-                    Boolean bool = rand.nextBoolean();
-                    if (bool){
-                        mMessages.add(new Message("ulternate", message, bool));
-                    } else {
-                        mMessages.add(new Message("kenneth", message, bool));
-                    }
-                    mMessageRecyclerAdapter.notifyDataSetChanged();
-                    hideSoftKeyboard(MainActivity.this, view);
-                    mTextField.setText("");
-                    mTextField.clearFocus();
-                }
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
             }
         });
-
     }
 
     @Override
@@ -100,10 +48,5 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public static void hideSoftKeyboard(Activity activity, View view){
-        InputMethodManager mInputMethodManager = (InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE);
-        mInputMethodManager.hideSoftInputFromWindow(view.getApplicationWindowToken(), 0);
     }
 }

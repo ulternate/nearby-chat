02bcfbcc6067faa95c178299b1084b503887030a
ulternate <contact@danielcswain.com>
2016-08-06 13:28:55 +0800

    Added timer and timertask to MainApplication to be used to check if app is in foreground or user is transitioning between applications. This will be used to disconnect from the GoogleApiClient when the app is in the background to conserve battery.

--- app/src/main/java/com/danielcswain/nearbychat/MainApplication.java
+++ app/src/main/java/com/danielcswain/nearbychat/MainApplication.java
@@ -2,12 +2,20 @@ package com.danielcswain.nearbychat;
 
 import android.app.Application;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 /**
  * Created by ulternate on 5/08/2016.
  */
 public class MainApplication extends Application {
 
     public static boolean firstStart = true;
+    public boolean wasInBackground;
+
+    private Timer mActivityTransitionTimer;
+    private TimerTask mActivityTransitionTimerTask;
+    private final long MAX_ACTIVITY_TRANSITION_TIME_MS = 2000;
 
     public MainApplication(){
 
@@ -21,5 +29,31 @@ public class MainApplication extends Application {
     public static void setFirstStart(boolean bool){
         firstStart = bool;
     }
+
+    public void startActivityTransitionTimer(){
+        this.mActivityTransitionTimer = new Timer();
+        this.mActivityTransitionTimerTask = new TimerTask() {
+            @Override
+            public void run() {
+                MainApplication.this.wasInBackground = true;
+                // Disconnect from the NearbyAPI client here
+            }
+        };
+
+        this.mActivityTransitionTimer.schedule(mActivityTransitionTimerTask, MAX_ACTIVITY_TRANSITION_TIME_MS);
+    }
+
+    public void stopActivityTransitionTimer(){
+        if (this.mActivityTransitionTimerTask != null){
+            this.mActivityTransitionTimerTask.cancel();
+        }
+
+        if (this.mActivityTransitionTimer != null){
+            this.mActivityTransitionTimer.cancel();
+        }
+
+        this.wasInBackground = false;
+    }
+
 
 }
